cd2d24ee62cd749246025d1777b84c58a34d287b
dotwolfwarrior <dww.2r1@gmail.com>
2025-03-27 12:49:53 -0400

    basic login page

--- wombats-app/src/pages/Login.jsx
+++ wombats-app/src/pages/Login.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
 const Login = () => {
+  const [login,setLogin] = useState("");
+  const [password,setPass] = useState("");
+  const [errorMSG,setError] = useState("");
+  const nav = useNavigate();
+
+  const check_login = (e) => {
+    e.preventDefault();
+    if(login == "Test" && password == "Test"){
+      nav("/"); // TODO make this check the auth and get the cookie starting
+    } else {
+      setError("login or password is inncorrect.")
+    }
+  }
+
   return (
     <div className="page">
       <h1>Login</h1>
-      <p>Login form will be implemented here.</p>
+      {/* <p>Login form will be implemented here.</p> */}
+      <form onSubmit={check_login}>
+        <input 
+          type="text"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <br/>
+        <input 
+          type="password"
+          value={password}
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <br/>
+        <button type="submit">Login</button>
+        {errorMSG ? (<p>{errorMSG}</p>) : ""}
+      </form>
     </div>
   );
 };
